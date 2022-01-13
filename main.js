e485e512c9fd9e44a76eb6cd685991b28a697f27
unknown <vlada.kitaeva2002@gmail.com>
2022-01-13 21:55:54 +0200

    created the view

---
+++ main.js
@@ -0,0 +1,69 @@
+import todos from './todos.js'
+const tbody = document.getElementById('notes-data')
+const categoriesBody = document.getElementById('categories-data')
+
+
+const renderNotes = () => {
+    const template = document.getElementById('notes-row') 
+    const row = template.content.querySelector('tr')
+    todos.forEach((todo, idx) => {
+        const node = document.importNode(row, true)
+        node.setAttribute('id', idx)
+        const columns = node.children
+        for (let i = 0; i < 5; i++) {
+            columns[i].innerText += todo[columns[i].className]
+        }
+        tbody.appendChild(node)
+    })
+}
+
+const calculateNotes = () => {
+    const nums = {
+        task: {
+            active: 0,
+            archived: 0
+        },
+        idea: {
+            active: 0,
+            archived: 0
+        },
+        thought: {
+            active: 0,
+            archived: 0
+        }
+    }
+
+    todos.forEach(el => {
+        switch(el.category) {
+            case "Task":
+                el.status === "active" ? nums.task.active += 1 : nums.task.archived += 1
+                break
+            case "Idea":
+                el.status === "active" ? nums.idea.active += 1 : nums.idea.archived += 1
+                break
+            case "Random thought":
+                el.status === "active" ? nums.thought.active += 1 : nums.thought.archived += 1
+                break
+        }
+    })
+    console.log(nums)
+
+    console.log(categoriesBody)
+    return nums
+}
+
+const renderCategoriesData = () => {
+    const nums = calculateNotes()
+    const rows = categoriesBody.children
+    console.log(rows)
+    for (let category in nums) {
+        const columns = rows[category].children
+        columns[1].innerText = nums[category].active
+        columns[2].innerText = nums[category].archived
+    }
+}
+
+window.addEventListener('load', (e) => {
+    renderNotes()
+    renderCategoriesData()
+})
---
+++ todos.js
@@ -0,0 +1,60 @@
+const todos = [
+    {
+        name: "Chores",
+        created: "06/01/2022",
+        category: "Task",
+        content: "Mow the lown",
+        dates: "",
+        status: "active"
+    },
+    {
+        name: "Final exams",
+        created: "01/09/2021",
+        category: "Random thought",
+        content: "Final exams are only final if you pass them",
+        dates: "",
+        status: "active"
+    },
+    {
+        name: "Jimmy's bday",
+        created: "10/01/2022",
+        category: "Task",
+        content: "Bake a cake",
+        dates: "",
+        status: "active"
+    },
+    {
+        name: "Books",
+        created: "10/01/2022",
+        category: "Task",
+        content: "Order books",
+        dates: "",
+        status: "active"
+    },
+    {
+        name: "Vacation destinations",
+        created: "25/12/2021",
+        category: "Idea",
+        content: "Thailand, Mallorca, Venice",
+        dates: "",
+        status: "active"
+    },
+    {
+        name: "Hyphenated",
+        created: "19/06/2021",
+        category: "Random thought",
+        content: "Hyphenated is non-hyphenated while non-hyphenated is hyphenated",
+        dates: "",
+        status: "active"
+    },
+    {
+        name: "History",
+        created: "14/01/2022",
+        category: "Task",
+        content: "Read p.117-125 for 04/02/2022",
+        dates: "04/02/2022",
+        status: "active"
+    },
+]
+
+export default todos
